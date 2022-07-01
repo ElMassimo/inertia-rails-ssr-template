5b662617d1d7a410ffec8dafa78bb907eef79145
Maximo Mussini <maximomussini@gmail.com>
2022-07-01 12:53:33 -0300

    feat: upgrade example to use Vite 3

--- app/frontend/ssr/ssr.jsx
+++ app/frontend/ssr/ssr.jsx
@@ -1,8 +1,11 @@
 import ReactDOMServer from 'react-dom/server'
 import { createInertiaApp } from '@inertiajs/inertia-react'
-import createServer from '@inertiajs/server'
+import cjsCreateServer from '@inertiajs/server'
 
 const pages = import.meta.globEagerDefault('../pages/*.jsx')
+
+// Unwrap the CJS module in @inertiajs/server.
+const createServer = typeof cjsCreateServer === 'function' ? cjsCreateServer : cjsCreateServer.default
 
 createServer((page) => createInertiaApp({
   page,
